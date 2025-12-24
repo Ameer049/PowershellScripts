a197cca8eb01f0aa54482b8600a9d161326bbb82
Ameer <147032936+Ameer049@users.noreply.github.com>
2025-12-24 19:17:54 +0100

    Add files via upload

---
+++ Azure/Gegevens apparaat ophalen.ps1
@@ -0,0 +1,35 @@
+﻿#Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
+
+
+# Laad de authentication-module handmatig
+#Import-Module Microsoft.Graph.Authentication
+
+# Laad de directory management module (voor apparaten)
+#Import-Module Microsoft.Graph.Identity.DirectoryManagement
+
+
+#cverbinding maken
+#Connect-MgGraph -Scopes "Device.Read.All"
+
+
+#ObjectID van je apparaat
+$ObjectId = "7840axx2-3410-4xab-8xx1-xxx"
+
+# Apparaatgegevens ophalen
+$device = Get-MgDevice -DeviceId $ObjectId
+
+# Toon alle eigenschappen overzichtelijk
+$device | Format-List *
+
+#$device.AdditionalProperties.GetEnumerator() | Format-List
+
+
+#Connect-MgGraph -Scopes "DeviceManagementServiceConfig.Read.All"
+
+#$deviceId = "your-azure-ad-device-object-id"
+
+#$autopilotDevices = Get-MgDeviceManagementWindowsAutopilotDeviceIdentity -All
+
+#$matchedDevice = $autopilotDevices | Where-Object { $_.azureAdDeviceId -eq $deviceId }
+
+#$matchedDevice | Select-Object id, serialNumber, deviceName, azureAdDeviceId
